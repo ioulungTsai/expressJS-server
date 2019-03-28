174f56ad2bfd4783d6e7e7b157028b49ca3c0bcb
ioulungTsai <brandontsai@ioulungTsai.local>
2019-03-27 21:59:15 -0700

    server with http methods

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const jsonServer = require('json-server');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+
+server.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+server.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+server.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+server.listen(3000, () =>
+  console.log(`Example server listening on port 3000!`),
+);
