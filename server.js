8745f53272c7ab73daf3105dbf8eb05475a885cb
brandonTsai <brandonya125@gmail.com>
2019-03-30 14:12:55 -0700

    update

--- server.js
+++ server.js
@@ -3,22 +3,8 @@ const jsonServer = require('json-server');
 
 const server = express();
 
-server.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
-});
-
-server.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
-});
-
-server.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
-});
-
-server.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
-});
+server.use('/api', jsonServer.router('db.json'))
 
 server.listen(3000, () =>
-  console.log(`Example server listening on port 3000!`),
-);
+  console.log(`Hola~ Listening!`),
+)
